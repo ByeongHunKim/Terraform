daa0542b8a9b2154286da30325833e0d6725b0ec
ByeongHunKim <rkwhr912@naver.com>
2024-05-31 16:34:49 +0900

    feat : eks module addons, eks_managed_node_groups

--- kubernetes/modules/eks/main.tf
+++ kubernetes/modules/eks/main.tf
@@ -1,19 +1,37 @@
+# https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
+
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
-  version = "19.20.0"
+  version = "~> 20.0"
 
   cluster_name = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id = var.vpc_id
   subnet_ids = var.subnet_ids
 
-  self_managed_node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity = 3
-      min_capacity = 1
-      instance_type = "t3.medium"
-      key_name = var.bastion_key_name
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  eks_managed_node_groups = {
+    terraform-node-group = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
     }
   }
 }
